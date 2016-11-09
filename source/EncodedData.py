6ca1cce2b93f0b6f087eeaf09e4a264629d60322
Matt Hartshorn <hartshmj@mail.uc.edu>
2016-11-09 17:38:25 -0500

    Initial Development
    + Created class/struct to store the data that will be converted to/from a binary string
    + Created the class/method structure for the main encoding class
    + Created the file that will contain the main function to run the script

---
+++ source/EncodedData.py
@@ -0,0 +1,7 @@
+
+
+class EncodedData:
+    
+    def __init__(self, chars_freqencies, byte_data):
+        self.chars_freqencies = chars_freqencies;
+        self.byte_data = byte_data;
---
+++ source/HuffmanEncoder.py
@@ -0,0 +1,52 @@
+import sys;
+from EncodedData import EncodedData;
+
+class HuffmanEncoder:
+    @staticmethod
+    def encode(data):
+        return;
+        
+    @staticmethod
+    def decode(bytes):
+        return;
+        
+# Private Methods
+    @staticmethod
+    def _getCharSetFrequency(data):
+        return;
+
+    @staticmethod
+    def _createTree(chars_frequencies):
+        return;
+        
+    @staticmethod
+    def _getBinaryMap(tree):
+        return;
+        
+        
+    @staticmethod
+    def _getEncodedData(bytes):
+        return;
+        
+    @staticmethod
+    def _decodeBytes(tree, data_bytes):
+        return;
+        
+        
+    @staticmethod
+    def _encodeDataToBytes(chars_frequencies, data):
+        return;
+        
+    @staticmethod
+    def _encodeDataToBytes(chars_frequencies, data):
+        return;
+        
+        
+    @staticmethod
+    def _bitStringToBytes(bits):
+        return int(bits, 2).to_bytes(len(bits) // 8, byteorder=sys.byteorder);
+    
+
+
+
+
